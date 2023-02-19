0ae85d87bc823b3358a0c5586e3142f1980edfee
kunal <kunalgoyalkanu@gmail.com>
2023-02-20 00:51:02 +0530

    updated form

--- src/components/Register.js
+++ src/components/Register.js
@@ -9,6 +9,11 @@ export default function Register() {
                 <input placeholder="name" type="text" />
             </div>
             <div className="formGroup">
+                <label>email</label>
+                <input placeholder="email" type="email" />
+            </div>
+            <div className="formGroup">
+                <label>password</label>
                 <input placeholder="password" type="password" />
             </div>
             <button type="submit">register</button>
